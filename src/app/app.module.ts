8bc065a0c8880b0174cd53f91d60496100d61396
nazarbekuly7 <tifixgame@gmail.com>
2023-09-17 23:11:48 +0600

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from "@angular/common/http";
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { UpdateuserComponent } from './components/updateuser/updateuser.component';
+import { ViewusersComponent } from './components/viewusers/viewusers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdduserComponent,
+    UpdateuserComponent,
+    ViewusersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
